4af464bd9af8a0221b24c4c86028a812321b8a91
ken <kenulborapublic@gmail.com>
2018-01-06 21:19:16 -0500

    template design

--- static/templates/myApiGateway.com/js/script.js
+++ static/templates/myApiGateway.com/js/script.js
@@ -4,14 +4,24 @@ var setM1 = function(){
     document.getElementById("m1").classList.add('active');
     document.getElementById("m2").classList.remove('active');
     document.getElementById("m3").classList.remove('active');
+    document.getElementById("m4").classList.remove('active');
 }
 var setM2 = function(){    
     document.getElementById("m2").classList.add('active');
     document.getElementById("m1").classList.remove('active');
     document.getElementById("m3").classList.remove('active');
+    document.getElementById("m4").classList.remove('active');
 }
 var setM3 = function(){
     document.getElementById("m3").classList.add('active');
     document.getElementById("m1").classList.remove('active');
     document.getElementById("m2").classList.remove('active');
+    document.getElementById("m4").classList.remove('active');
+}
+
+var setM4 = function(){
+    document.getElementById("m4").classList.add('active');
+    document.getElementById("m1").classList.remove('active');
+    document.getElementById("m2").classList.remove('active');
+    document.getElementById("m3").classList.remove('active');
 }
