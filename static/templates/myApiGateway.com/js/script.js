92b240efa88ff2c84c65c6ceacd51ce9b4c2136f
ken <kenulborapublic@gmail.com>
2018-01-14 18:14:30 -0500

    changes

--- static/templates/myApiGateway.com/js/script.js
+++ static/templates/myApiGateway.com/js/script.js
@@ -1,27 +1,73 @@
 
 
-var setM1 = function(){
-    document.getElementById("m1").classList.add('active');
-    document.getElementById("m2").classList.remove('active');
-    document.getElementById("m3").classList.remove('active');
-    document.getElementById("m4").classList.remove('active');
+//var msel;
+var challengeUrl = "http://localhost:3003/rs/challenge/en_us";
+
+
+var setM1 = function () {
+    localStorage.setItem("msel", "m1");
 }
-var setM2 = function(){    
-    document.getElementById("m2").classList.add('active');
-    document.getElementById("m1").classList.remove('active');
-    document.getElementById("m3").classList.remove('active');
-    document.getElementById("m4").classList.remove('active');
+var setM2 = function () {
+    localStorage.setItem("msel", "m2");
+
 }
-var setM3 = function(){
-    document.getElementById("m3").classList.add('active');
-    document.getElementById("m1").classList.remove('active');
-    document.getElementById("m2").classList.remove('active');
-    document.getElementById("m4").classList.remove('active');
+var setM3 = function () {
+    localStorage.setItem("msel", "m3");
 }
 
-var setM4 = function(){
-    document.getElementById("m4").classList.add('active');
-    document.getElementById("m1").classList.remove('active');
-    document.getElementById("m2").classList.remove('active');
-    document.getElementById("m3").classList.remove('active');
+var setM4 = function () {
+    localStorage.setItem("msel", "m4");
 }
+
+var setMenu = function () {
+    var msel = localStorage.getItem("msel");
+    if (msel === "m1") {
+        document.getElementById("m1").classList.add('active');
+        document.getElementById("m2").classList.remove('active');
+        document.getElementById("m3").classList.remove('active');
+        document.getElementById("m4").classList.remove('active');
+    } else if (msel === "m2") {
+        document.getElementById("m2").classList.add('active');
+        document.getElementById("m1").classList.remove('active');
+        document.getElementById("m3").classList.remove('active');
+        document.getElementById("m4").classList.remove('active');
+    } else if (msel === "m3") {
+        document.getElementById("m3").classList.add('active');
+        document.getElementById("m1").classList.remove('active');
+        document.getElementById("m2").classList.remove('active');
+        document.getElementById("m4").classList.remove('active');
+    } else if (msel === "m4") {
+        document.getElementById("m4").classList.add('active');
+        document.getElementById("m1").classList.remove('active');
+        document.getElementById("m2").classList.remove('active');
+        document.getElementById("m3").classList.remove('active');
+    }
+}
+
+var loadChallenge = function(){
+    var msel = localStorage.getItem("msel");
+    if (msel === "m4") {
+        $.ajax({ 
+            type: "GET",
+            dataType: "json",
+            url: challengeUrl,
+            success: function(data){        
+              //alert(data);
+              document.getElementById("question").value = data.question;
+              document.getElementById("key").value = data.key;
+            },
+            error: function(err){
+                console.log(JSON.stringify(err))
+            }
+         });
+        
+    }
+}
+
+
+
+$(function () {
+    $('.ui.checkbox')
+        .checkbox()
+        ;
+});
