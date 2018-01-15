b378a8ed72768f8d6cd91e840130b9e95b41c7bf
ken <kenulborapublic@gmail.com>
2018-01-14 19:45:17 -0500

    changes

--- static/templates/myApiGateway.com/js/script.js
+++ static/templates/myApiGateway.com/js/script.js
@@ -19,6 +19,10 @@ var setM4 = function () {
     localStorage.setItem("msel", "m4");
 }
 
+var setM5 = function () {
+    localStorage.setItem("msel", "m5");
+}
+
 var setMenu = function () {
     var msel = localStorage.getItem("msel");
     if (msel === "m1") {
@@ -26,21 +30,32 @@ var setMenu = function () {
         document.getElementById("m2").classList.remove('active');
         document.getElementById("m3").classList.remove('active');
         document.getElementById("m4").classList.remove('active');
+        document.getElementById("m5").classList.remove('active');
     } else if (msel === "m2") {
         document.getElementById("m2").classList.add('active');
         document.getElementById("m1").classList.remove('active');
         document.getElementById("m3").classList.remove('active');
         document.getElementById("m4").classList.remove('active');
+        document.getElementById("m5").classList.remove('active');
     } else if (msel === "m3") {
         document.getElementById("m3").classList.add('active');
         document.getElementById("m1").classList.remove('active');
         document.getElementById("m2").classList.remove('active');
         document.getElementById("m4").classList.remove('active');
+        document.getElementById("m5").classList.remove('active');
     } else if (msel === "m4") {
         document.getElementById("m4").classList.add('active');
         document.getElementById("m1").classList.remove('active');
         document.getElementById("m2").classList.remove('active');
         document.getElementById("m3").classList.remove('active');
+        document.getElementById("m5").classList.remove('active');
+    }
+    else if (msel === "m5") {
+        document.getElementById("m5").classList.add('active');
+        document.getElementById("m1").classList.remove('active');
+        document.getElementById("m2").classList.remove('active');
+        document.getElementById("m3").classList.remove('active');
+        document.getElementById("m4").classList.remove('active');
     }
 }
 
